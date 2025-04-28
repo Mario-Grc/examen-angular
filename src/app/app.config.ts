f1b7950f26b4c3cd6ef26fa8f91ce51513106fc0
mario <mario2004mario16@hotmail.com>
2025-04-28 16:49:28 +0100

    Todo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "examenangular-db543", appId: "1:724386627907:web:c8c05ce4ad12a9fa186ff6", storageBucket: "examenangular-db543.firebasestorage.app", apiKey: "AIzaSyBNSZntnrRGOTqZ72MmfBDxLNI1RRb95bo", authDomain: "examenangular-db543.firebaseapp.com", messagingSenderId: "724386627907" })), provideFirestore(() => getFirestore())]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {EmployeeFormComponent} from './components/employee-form/employee-form.component';
+import {ProjectFormComponent} from './components/project-form/project-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: EmployeeFormComponent},
+  {path: 'project', component: ProjectFormComponent},
+];
